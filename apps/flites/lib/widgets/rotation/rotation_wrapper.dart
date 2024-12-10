127484f57ec598076d3e954490e434a343305ee4
Jacob Aschenwald <jacob.aschenwald@gmail.com>
2024-12-10 18:34:19 +0100

    chore: remove prints and comments

--- apps/flites/lib/widgets/rotation/rotation_wrapper.dart
+++ apps/flites/lib/widgets/rotation/rotation_wrapper.dart
@@ -16,9 +16,7 @@ class RotationWrapper extends StatefulWidget {
 }
 
 class _RotationWrapperState extends State<RotationWrapper> {
-  double rotation =
-      0.0; // Accumulated rotation angle (needed so it does not flip)
-  // Offset _center = Offset.zero;
+  double rotation = 0.0;
   Offset dragStartPoint = Offset.zero;
 
   late final double circleRadius;
@@ -26,34 +24,22 @@ class _RotationWrapperState extends State<RotationWrapper> {
   @override
   void initState() {
     super.initState();
-    // _center = widget.rect.center;
     circleRadius = (longestSideSize(widget.rect.size) / 2) * 1.5;
     dragStartPoint = Offset(0, circleRadius);
-
-    print('Original drag start point: $dragStartPoint');
   }
 
   double calculateAngle(Offset start, Offset end) {
     double startAngle = atan2(start.dy, start.dx);
     double endAngle = atan2(end.dy, end.dx);
 
-    // Find the difference in angles, normalized to be within [-pi, pi]
     double angleDifference = endAngle - startAngle;
 
-    // print(
-    //     'Angle between start ( $start ) and end ( $end ) is: $angleDifference')t;
     return angleDifference;
   }
 
   void _updateRotation(Offset currentPosition) {
     final newAngle =
         calculateAngle(Offset(0, 1), dragStartPoint + flipY(currentPosition));
-    // print('From position: $dragStartPoint');
-    // print('To position: ${dragStartPoint + flipY(currentPosition)}');
-    // print('Angular Change in degrees: ${newAngle * 180 / pi}');
-    // print('New rotation: ${dragStartPoint.direction - newAngle - pi / 2}');
-
-    print('Current angle: ${rotation * 180 / pi}');
 
     setState(() {
       rotation = -newAngle;
@@ -61,33 +47,12 @@ class _RotationWrapperState extends State<RotationWrapper> {
   }
 
   void updateStartPoint(Offset endPosition) {
-    // final currentStartPosition = dragStartPoint;
     final endAngle =
         calculateAngle(dragStartPoint, dragStartPoint + flipY(endPosition));
-    final oldPoint = dragStartPoint;
 
-    final angleInDegrees = endAngle * 180 / pi;
-    dragStartPoint = rotateOffset(dragStartPoint, endAngle);
-
-    // final bestNewStartPoint = scaleOffset(
-    //     dragStartPoint + flipY(endPosition), widget.rect.height / 2);
-
-    // final rotatedOffset = rotateOffset(dragStartPoint, endAngle);
-
-    final newPoint = dragStartPoint;
-    print('hi');
-    print('new start point: $newPoint');
-    setState(() {});
-//
-    // final standardizedEndPosition = standardizeOffsetToRotation(endPosition);
-
-    // final endPoint = pointFromAngleAndLength(endAngle, widget.rect.height / 2);
-
-    // /// No need to call setState as this is only needed for calculations
-    // dragStartPoint = endPoint;
-    // final angle = -(dragStartPoint.direction - pi / 2);
-    // print('Angle in degrees: ${angle * 180 / pi}');
-    // print('Start Drag Point: $dragStartPoint');
+    setState(() {
+      dragStartPoint = rotateOffset(dragStartPoint, endAngle);
+    });
   }
 
   @override
@@ -113,14 +78,11 @@ class _RotationWrapperState extends State<RotationWrapper> {
             Container(
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-
                 border: Border.all(
                   color: const Color.fromARGB(255, 102, 102, 102),
                   width: 2,
                 ),
-                // color: const Color.fromARGB(255, 117, 174, 255),
               ),
-              // height: longestSideSize(widget.rect.size),
               width: circleRadius * 2 - (dotSize * 2 / 3),
             ),
             Positioned(
@@ -129,11 +91,8 @@ class _RotationWrapperState extends State<RotationWrapper> {
                 cursor: SystemMouseCursors.precise,
                 child: GestureDetector(
                   behavior: HitTestBehavior.opaque,
-                  onPanStart: (details) {
-                    // Initialize the start vector
-                  },
+                  onPanStart: (details) {},
                   onPanUpdate: (details) {
-                    // Update the rotation based on the current position
                     _updateRotation(
                         standardizeOffsetToRotation(details.localPosition));
                   },
@@ -160,7 +119,6 @@ class _RotationWrapperState extends State<RotationWrapper> {
 
   Offset standardizeOffsetToRotation(Offset offset) {
     final angle = -(dragStartPoint.direction - pi / 2);
-    // print('Angle in degrees: ${angle * 180 / pi}');
 
     double x = offset.dx * cos(angle) - offset.dy * sin(angle);
     double y = offset.dx * sin(angle) + offset.dy * cos(angle);
