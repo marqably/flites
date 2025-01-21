d71b693dd057f05a4924a64b3d49d17d90071df9
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2025-01-21 09:47:47 +0100

    feat: export dialog and customization

---
+++ apps/flites/lib/widgets/export/export_button.dart
@@ -0,0 +1,21 @@
+import 'package:flites/widgets/buttons/stadium_button.dart';
+import 'package:flites/widgets/export/export_dialog_content.dart';
+import 'package:flites/widgets/project_file_list/overlay_button.dart';
+import 'package:flutter/material.dart';
+
+class ExportButton extends StatelessWidget {
+  const ExportButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OverlayButton(
+      followerAnchor: Alignment.bottomRight,
+      targetAnchor: Alignment.topRight,
+      offset: Offset(0, 50),
+      buttonChild: StadiumButton(
+        text: 'Export Sprite',
+      ),
+      overlayContent: ExportDialogContent(),
+    );
+  }
+}
---
+++ apps/flites/lib/widgets/export/file_path_picker.dart
@@ -0,0 +1,76 @@
+import 'package:flites/main.dart';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FilePathPicker extends StatefulWidget {
+  const FilePathPicker({
+    super.key,
+    required this.onPathSelected,
+    this.initialPath,
+  });
+
+  final void Function(String path) onPathSelected;
+  final String? initialPath;
+
+  @override
+  State<FilePathPicker> createState() => _FilePathPickerState();
+}
+
+class _FilePathPickerState extends State<FilePathPicker> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _initDefaultPath();
+  }
+
+  Future<void> _initDefaultPath() async {
+    final downloadsDir = await getDownloadsDirectory();
+    if (downloadsDir != null) {
+      _controller.text = downloadsDir.path;
+      widget.onPathSelected(downloadsDir.path);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickDirectory() async {
+    final result = await FilePicker.platform.getDirectoryPath();
+
+    if (result != null) {
+      _controller.text = result;
+      widget.onPathSelected(result);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              isDense: true,
+              filled: true,
+              fillColor: context.colors.surface,
+            ),
+            onChanged: widget.onPathSelected,
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.folder_open_outlined),
+          onPressed: _pickDirectory,
+        ),
+      ],
+    );
+  }
+}
