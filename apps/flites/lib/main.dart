337b00898f23d8dd1517754092a419b59d2fe820
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2025-02-04 16:59:03 +0100

    feat: menu bar with app preferences
    - theme (light, dark, system)
    - language (en, de, es)

--- apps/flites/lib/main.dart
+++ apps/flites/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flites/screens/overview.dart';
+import 'package:flites/states/app_settings.dart';
 import 'package:flites/theme/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flites/widgets/menu_bar/app_menu_bar.dart';
+import 'package:signals/signals_flutter.dart';
 
 void main() {
   runApp(const FlitesApp());
@@ -13,25 +16,30 @@ class FlitesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('es'),
-        Locale('de'),
-      ],
-      themeMode: ThemeMode.light,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      home: const Scaffold(
-        body: Overview(),
-      ),
-    );
+    return Watch((context) {
+      return MaterialApp(
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'),
+          Locale('es'),
+          Locale('de'),
+        ],
+        locale: AppSettings.currentLocale,
+        themeMode: AppSettings.themeMode,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: const AppMenuBar(
+          child: Scaffold(
+            body: Overview(),
+          ),
+        ),
+      );
+    });
   }
 }
 
