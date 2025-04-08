c3b43e45953bda52d676b509cd397ece39e306f2
Jacob Aschenwald <jacob.aschenwald@gmail.com>
2025-04-08 19:31:21 +0200

    feat: select new reference image when changing image row selection

--- apps/flites/lib/states/selected_image_row_state.dart
+++ apps/flites/lib/states/selected_image_row_state.dart
@@ -1,3 +1,5 @@
+import 'package:flites/states/selected_image_state.dart';
+import 'package:flites/states/source_files_state.dart';
 import 'package:signals/signals_flutter.dart';
 
 /// Defines what row of images is currently being edited
@@ -7,6 +9,14 @@ ReadonlySignal<int> get selectedImageRow => _selectedImageRow.readonly();
 
 class SelectedImageRowState {
   static void setSelectedImageRow(int index) {
+    if (projectSourceFiles.value.rows[index].images.isNotEmpty) {
+      SelectedImageState.setSelectedImage(
+        projectSourceFiles.value.rows[index].images.first.id,
+      );
+    } else {
+      SelectedImageState.setSelectedImage(null);
+    }
+
     _selectedImageRow.value = index;
   }
 }
